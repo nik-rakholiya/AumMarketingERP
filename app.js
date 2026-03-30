a767657bfa12afe6151f7b13cd573bd5f5055aae
nik-rakholiya <134193713+nik-rakholiya@users.noreply.github.com>
2026-03-30 11:04:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
    CONFIG
 ========================================================================= */
 // Replace this with the URL you get after deploying your Google Apps Script Web App
-const APPS_SCRIPT_WEB_APP_URL = "YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APPS_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxjjfWCQbENMnqCL1FzDKa3Irp6pCHqiwcIMYIzs1E/dev";
 
 /* =========================================================================
    CORE STATE & INITIALIZATION
